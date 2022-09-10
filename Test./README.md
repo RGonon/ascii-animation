77a11c6734521b8dc63ba45b29cc8258585e111e
natch <raphael.gonon18@gmail.com>
2022-09-10 14:06:57 +0200

    add test. project

---
+++ Test./README.md
@@ -0,0 +1,8 @@
+##Simple launch 
+`npx votaitest.`
+
+##Used packages 
+- figlet (https://github.com/patorjk/figlet.js)
+- gradient-string (https://github.com/bokub/gradient-string)
+
+`npm i figlet gradient-string`
---
+++ Test./index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+var figlet = require('figlet');
+var gradient = require('gradient-string');
+
+var txt = "VotaiTest.";
+var text =""
+
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function delaywritting(){
+        while (true) {
+                for (let i = 0; i < txt.length; i++) {
+                        await sleep(200);
+                        text+=txt[i];
+                        var message = figlet.textSync(text, {
+                                horizontalLayout: 'default',
+                                verticalLayout: 'default',
+                                width: 60,
+                                whitespaceBreak: true
+                         });
+                        console.clear();
+                        console.log(gradient('cyan', 'pink') (message));
+                }
+                await sleep(1000);
+                text = "";
+                console.clear();
+        }
+}
+
+delaywritting();
